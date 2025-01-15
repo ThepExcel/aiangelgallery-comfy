04e64714cd62965c43d222bc16e97912306a4784
ThepExcel <thepexcel@gmail.com>
2025-01-15 14:48:50 +0700

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .ThepExcel_aiangel import MultilineTextChoiceNode
+
+NODE_CLASS_MAPPINGS = {
+    "ThepExcel_AiAngel_MultilineTextChoiceNode": MultilineTextChoiceNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ThepExcel_AiAngel_MultilineTextChoiceNode": "ThepExcel_AiAngel_Multiline Text Choice"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
